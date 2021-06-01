47ff1b28c5605f2315486150f7386c02c47081f0
Leonardo Felix Scandura <Leo@192.168.0.105>
2021-06-01 14:58:47 -0300

    Quarto Exercicio - corrigido

--- src/ex4/Main.java
+++ src/ex4/Main.java
@@ -3,13 +3,21 @@ package ex4;
 public class Main {
 
 	public static void main(String[] args) {
-		Pair<String, String> p = new Pair<>("Cod1", "String");
-		Pair<String, String> p2 = new Pair<>("Cod2", "String2");
+		Pair<String, String> p1 = new Pair<>("Cod1", "Agua");
+		Pair<String, String> p2 = new Pair<>("Cod2", "Cafe");
+		Pair<String, String> p3 = new Pair<>("Cod2", "Refrigerante");
 		
-		Pairs lista = new Pairs();
+		Pairs<String, String> lista = new Pairs<>();
 		
-		lista.addPair(p);
-		lista.addPair(p2);
+		System.out.println("Testes ADD:");
+		System.out.println(lista.addPair(p1));
+		System.out.println(lista.addPair(p2));
+		System.out.println(lista.addPair(p3));
+		
+		System.out.println("\nTestes Busca:");
+		System.out.println(lista.buscaKey("Cod2"));
+		System.out.println(lista.buscaKey("Cod3"));
+		
 
 	}
 
--- src/ex4/Pairs.java
+++ src/ex4/Pairs.java
@@ -1,6 +1,7 @@
 package ex4;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Pairs <K,V>{
 	
@@ -8,18 +9,41 @@ public class Pairs <K,V>{
 	private ArrayList<Pair<K, V>> ListaDPairs = new ArrayList<>();
 	
 
-	@SuppressWarnings("hiding")
-	public <K, V> boolean addPair(Pair<K, V> p1) {
+
+	public boolean addPair(Pair<K, V> p1) {
 		int med = 0;
-		System.out.println(p1);
 		
-			for (Pair<K, V> pair : ListaDPairs) {			
-				if(Util.compare(p1, pair)) {
-					med++;
-				}
-			}	
+		if(ListaDPairs.isEmpty()) {
+			ListaDPairs.add(p1);	
+			return true;
+		} else {
+			for (Iterator iterator = ListaDPairs.iterator(); iterator.hasNext();) {	
+				Pair<K, V> pair = (Pair<K, V>) iterator.next();
+					if(Util.compare(p1, pair)) {
+						med++;
+					}
+			}
+			if (med > 0) {
+				return false;
+			} else {
+				ListaDPairs.add(p1);
+				return true;
+			}
+		}
 		
-		System.out.println(med);
-		return true;
 	}
+	
+	public Pair<K, V> buscaKey(K KeyV1){
+		for (Iterator iterator = ListaDPairs.iterator(); iterator.hasNext();) {	
+			Pair<K, V> pair = (Pair<K, V>) iterator.next();
+			if(pair.getKey().equals(KeyV1)) {
+				return pair;
+			} 
+		}
+		return null;
+	}
+	
+	
+	
+	
 }
